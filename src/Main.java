50bc2b7931cc96cec7dcddcddb92a9a72870cb5c
Hannah <hannah.rabelo@gmail.com>
2022-08-15 21:43:02 -0300

    Finalizando Desafio

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import java.time.LocalDate;
 
-import br.com.dio.desafio.dominio.Conteudo;
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
@@ -16,20 +17,54 @@ public class Main {
 		Curso curso2 = new Curso();
 		curso2.setTitulo("curso js");
 		curso2.setDescricao("descricao curso de js");
-		curso2.setCargaHoraria(4);
-		
-		System.out.println(curso1);
-		System.out.println(curso2);
+		curso2.setCargaHoraria(4);		
 		
 		Mentoria mentoria1 = new Mentoria();
 		mentoria1.setTitulo("mentoria java");
 		mentoria1.setDescricao("descricao mentoria java");
 		mentoria1.setData(LocalDate.now());
 		
-		System.out.println(mentoria1);
-		
+		//System.out.println(curso1);
+		//System.out.println(curso2);
+		//System.out.println(mentoria1);		
 		//Polimorfismo
 		//Conteudo conteudo = new Curso();
+		
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setNome("Bootcamp Java Developer");
+		bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+		bootcamp.getConteudos().add(curso1);
+		bootcamp.getConteudos().add(curso2);
+		bootcamp.getConteudos().add(mentoria1);
+		
+		Dev devCamila = new Dev();
+		devCamila.setNome("Camila");
+		devCamila.inscreverBootcamp(bootcamp);
+		System.out.println("Conteúdos Incritos Camila: " + devCamila.getConteudosInscritos());
+		devCamila.progredir();
+		devCamila.progredir();
+		System.out.println("-");
+		System.out.println("Conteúdos Incritos Camila: " + devCamila.getConteudosInscritos());
+		System.out.println("Conteúdos Concluidos Camila: " + devCamila.getConteudosConcluidos());
+		System.out.println("XP: " + devCamila.calcularTotalXp());
+		
+		System.out.println("---------");
+		
+		Dev devJoao = new Dev();
+		devJoao.setNome("João");
+		devJoao.inscreverBootcamp(bootcamp);
+		System.out.println("Conteudos Inscritos João: " + devJoao.getConteudosInscritos());
+		devJoao.progredir();
+		devJoao.progredir();
+		devJoao.progredir();
+		System.out.println("-");
+		System.out.println("Conteudos Inscritos João: " + devJoao.getConteudosInscritos());
+		System.out.println("Conteudos Concluidos João: " +devJoao.getConteudosConcluidos());
+		System.out.println("XP: " + devJoao.calcularTotalXp());
+
+		
+		
+		
 
 	}
 
--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -2,26 +2,32 @@ package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
 	
 	private String Nome;
 	private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
-	private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
-	
-	//LinkedHashSet só permite adicionar elementos únicos
+	private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>(); //LinkedHashSet só permite adicionar elementos únicos
 	
 	public void inscreverBootcamp(Bootcamp bootcamp) {
-		
+		this.conteudosInscritos.addAll(bootcamp.getConteudos());
+		bootcamp.getDevsInscritos().add(this);
 	}
 	
 	public void progredir() {
-		
+		Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+		if(conteudo.isPresent()) {
+			this.conteudosConcluidos.add(conteudo.get());
+			this.conteudosInscritos.remove(conteudo.get());
+		}else {
+			System.err.println("Você não está matriculado em nenhum conteudo!");
+		}
 	}
 	
-	public void calcularTotalXp() {
-		
+	public double calcularTotalXp() {
+		return this.conteudosConcluidos.stream().mapToDouble(conteudo -> conteudo.calcularXp()).sum();
 	}
 
 	public String getNome() {
